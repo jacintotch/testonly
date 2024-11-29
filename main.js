9a6fdc5437190570d2ddacc60762adfd655e6bf0
Jacinto Celestino Tchayevala <celestinojcj@gmail.com>
2024-11-29 10:13:08 +0200

    phone version

--- main.js
+++ main.js
@@ -8,3 +8,38 @@ var swiper = new Swiper(".mySwiper", {
     },
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburger = document.querySelector('.hamburger');
+  const navMenu = document.querySelector('#nav-menu');
+
+  // Toggle the visibility of the menu when the hamburger icon is clicked
+  hamburger.addEventListener('click', function () {
+      navMenu.classList.toggle('active');
+  });
+});
+
+
+$(document).ready(function(){
+  $('.carousel').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000, // Transition every 3 seconds
+      arrows: true,
+      dots: true, // Optional: add navigation dots
+      responsive: [
+          {
+              breakpoint: 768,
+              settings: {
+                  slidesToShow: 2
+              }
+          },
+          {
+              breakpoint: 480,
+              settings: {
+                  slidesToShow: 1
+              }
+          }
+      ]
+  });
+});
